1b6f8c331416fcb593e2996c5cd1c76ce5378b58
ainmzlnn <ain.mazlan19@gmail.com>
2020-11-30 16:52:30 +0800

    30 nov 2020

---
+++ lib/main.dart
@@ -0,0 +1,87 @@
+//import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Home()
+  ));
+
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int item = 0;
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+      appBar: AppBar(
+      title: Text('Catalog Item'),
+      centerTitle: true,
+      backgroundColor: Colors.pink[300],
+    ),
+    body: Padding(
+      padding: const EdgeInsets.fromLTRB(0.0, 200.0, 0.0, 0.0),
+      child: Column (children: <Widget>[
+          Center (
+            child: Text('My Item',
+            style: TextStyle(
+              fontSize: 30.0,
+              fontWeight: FontWeight.bold,
+              color: Colors.pink[900],
+              fontFamily: 'Courgette'
+              ),
+              ),
+         ),
+         Center (
+            child: Text(
+              '$item',
+            style: TextStyle(
+              fontSize: 40.0,
+              fontWeight: FontWeight.bold,
+              color: Colors.pink[900]
+              ),
+              ),
+         ),
+
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 0.0, vertical: 50.0),
+            child: Center (
+              child: RaisedButton.icon(
+                onPressed: () {
+                  setState(() {
+                    item = 0;
+                  });
+                },
+                icon: Icon(Icons.restore, color: Colors.white,),
+                label: Text ('restore item',
+                style: TextStyle (color: Colors.white, fontSize: 20.0),),
+                color: Colors.amber[600],
+                ),
+            ),
+          ),
+
+      ],
+      )
+      
+      
+    ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            item++;
+          });
+        },
+        child: Icon(
+          Icons.add
+        ),
+        backgroundColor: Colors.pink,
+      ),
+      );
+    }
+}
+
